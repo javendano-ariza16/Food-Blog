bf0952291a0ea96a8d3b2acc6ee5e74bdab13aed
David Avedano <javendano-ariza@myseneca.ca>
2023-02-17 22:03:41 -0500

    assigment3

--- blog-service.js
+++ blog-service.js
@@ -1,26 +1,138 @@
+/*********************************************************************************
+* WEB322 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Juan David Avendao Student ID: 165095217 Date: 17/02/2023
+*
+* Cyclic Web App URL: https://motionless-jay-pullover.cyclic.app
+*
+* GitHub Repository URL: https://github.com/javendano-ariza16/web322-app
+*
+********************************************************************************/ 
+const e = require("express");
 const fs = require("fs"); 
 var post =[];
 var categories = [];
 
 exports.initialize = () => {
     return new Promise ((resolve, reject) => {
-        file.readFile('./data/categories.json', (err,data) => {
+        fs.readFile('./data/categories.json', 'utf8',(err,data) => {
             if (err) {
                 reject ('unable to read file');
             }
             else {
-                employees = JSON.parse(data);
+                categories = JSON.parse(data);
             }
         });
 
-        file.readFile('./data/departments.json', (err,data)=> {
+        fs.readFile('./data/post.json', (err,data)=> {
             if (err) {
                 reject ('unable to read file');
             }
             else {
-                departments = JSON.parse(data);
+                post = JSON.parse(data);
             }
         })
         resolve();
+    }) 
+};
+
+exports.getAllPosts = () => {
+    return  new Promise((resolve, reject) => {
+        
+       if(post.length === 0) {
+        let err = "No results returned";
+        reject({message: err});
+       }  
+
+    resolve (post);
     })
 };
+
+exports.getPublishedPosts = () => {
+    return new Promise((resolve,reject) => {
+    var publishedPost =[];
+    for(let i=0;i<post.length;i++){
+        if(post[i].published===true)
+        {publishedPost.push(post[i]);}
+    }
+    if(publishedPost.length===0)
+    {
+        let err= "no results return "
+        reject({message:err});
+    }
+    resolve(publishedPost);
+    })
+}
+
+exports.getCategories = () =>{
+    return  new Promise((resolve, reject) => {
+        
+        if(categories.length === 0) {
+         let err = "No results returned";
+         reject({message: err});
+        }  
+ 
+     resolve (categories);
+     })
+}
+
+//addPost(postData) 
+module.exports.addPost = function (postData){
+    var promise = new Promise((resolve, reject) =>{
+        if (postData.published === undefined) {
+            postData.published = false;
+        } else postData.published = true;
+        
+        postData.id = posts.length + 1;
+        postData.postDate = ConvertJsonDateString(Date.now());
+        postData.category = parseInt(postData.category);
+        posts.push(postData);
+        // reject("postData is undefined!");
+        resolve(postData);
+    })
+    return promise;
+}
+
+//queryPosts by category
+module.exports.getPostsByCategory = (category) => {
+    console.log('query Post by Category starts!');
+    var min_category = 1;
+    var max_category = categories.length;
+    let query_post_category = [];
+    var promise = new Promise((resolve, reject) => { 
+        for ( let i = 0; i < posts.length; i++){
+            if (posts[i].category == category) query_post_category.push(posts[i]);
+        }
+        if (category < min_category || category > max_category ) // tell client no result cos category value out of bound
+            reject(`No result returned in this query! Hint: Your category value seems out of range! Please choose ${min_category} - ${max_category}!`);
+        if (query_post_category.length === 0 ) // tell client no result cos really no query result with correct inputted category value
+            reject(`No result returned in this query with your input category value ${category}!`);
+        else resolve(query_post_category);
+    })
+    console.log('query Post by Category ends!');
+    return promise;
+}
+
+//queryPosts by minDate
+module.exports.getPostsByMinDate = (minDateStr) => {
+    console.log('query Post by minDate starts');
+    var regex = /^[0-9]{4}[\-\/\.][0-9]{1,2}[\-\/\.][0-9]{1,2}$/g; // check the input date format
+    let query_post_date = [];
+    var promise = new Promise((resolve, reject) => {
+        if (regex.test(minDateStr)){
+            for ( let i = 0; i < posts.length; i++){
+                if (new Date(posts[i].postDate) >= new Date(minDateStr)) query_post_date.push(posts[i]);
+            }
+            if (query_post_date.length === 0 )
+                reject(`No result returned in this query with your input date ${minDateStr}!`); // data input format correct but no result 
+            else    
+                resolve(query_post_date);
+        }else   // wrong format of date
+            reject('No result returned! Please enter your query in such date format YYYY-MM-DD OR YYYY.MM.DD e.g. 2020-12-01 , 2020.12.01' );
+    });
+    console.log('query Post by minDate ends');
+    return promise;
+}
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
+/*********************************************************************************
+* WEB322 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Juan David Avendao Student ID: 165095217 Date: 17/02/2023
+*
+* Cyclic Web App URL: https://motionless-jay-pullover.cyclic.app
+*
+* GitHub Repository URL: https://github.com/javendano-ariza16/web322-app
+*
+********************************************************************************/ 
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 var express = require("express");
 var app = express();
 var path = require("path");
 const blog_service = require("./blog-service.js");
 
+
 var HTTP_PORT = process.env.PORT || 8080;
+cloudinary.config({
+  cloud_name: 'dvnbtindj',
+  api_key: '768353363744276',
+  api_secret: 'j9Y7fbsWM9rVa7rFmtsgzVwLZ_c',
+  secure: true
+ });
+ 
 
 // call this function after the http server starts listening for requests
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
+
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+
 //add static middleware
 app.use(express.static('public'));
 
@@ -17,30 +44,118 @@ app.get("/", (req, res) => {
     res.redirect("/about");
 });
 
-app.get("/", function (req, res) {
+app.get("/",  (req, res)=> {
     res.sendFile(path.join(__dirname, "views/about.html"));
   });
   
-  app.get("/about", function (req, res) {
+  app.get("/about", (req, res)=> {
     res.sendFile(path.join(__dirname, "views/about.html"));
   });
 
   app.get("/blog",(req,res)=>
   res.send("<h1>TODO: get all posts who have published==true</h1>"));
 
-  app.get("/posts",(req,res)=>
-  res.send("<h1>TODO:This route will return a JSON formatted string containing all the posts within the posts.json files</h1>"));
+  app.get("/posts", (req, res) => {
+    let category = req.query.category;
+    let min_date = req.query.minDate;
+    if (category){
+        blog_service.getPostsByCategory(category).then((dataC) => {res.json(dataC);}) // send json data back
+        .catch((err) => {
+        console.log(err);
+        res.json(err);
+        })
+    }
+    else if (min_date) {
+        blog_service.getPostsByMinDate(min_date).then((dataD) => {res.json(dataD);}) //send json data back
+        .catch((err) => {
+            console.log(err);
+            res.json(err);
+        }); 
+    } else { // 
+        blog_service.getAllPosts()
+        .then((data) =>{
+            console.log("getAllPosts JSON loaded!");
+            res.json(data);
+        })
+        .catch((err) =>{
+            console.log(err);
+            res.json(err);
+        })
+    }
+});
 
-  app.get("/categories",(req,res)=>
-  res.send("<h1>TODO:• This route will return a JSON formatted string containing all the categories within the categories.json file</h1>"));
+// add post/value route
+app.get("/post/:id", (req, res) =>{
+  let id = req.params.id;
+  blog_service.getPostById(id)
+  .then((dataI) => {res.json(dataI);})
+  .catch((err) => {
+      console.log(err);
+      res.json(err);
+  })
+});
 
+
+    app.get("/categories", (req, res) => {
+      blog_service.getCategories().then((data) => {
+          res.json({data});
+      }).catch((err) => {
+          res.json({message: err});
+      })
+  });
+
+  app.get("/posts/add", (req, res)=> {
+    res.sendFile(path.join(__dirname, "views/addPost.html"));
+  });
+
+
+
+  app.post("/post/add",upload.single("featureImage"), (req,res)=>{
+    if(req.file){
+      let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+          let stream = cloudinary.uploader.upload_stream(
+            (error, result) => {
+              if (result) {
+                resolve(result);
+              } else {
+                reject(error);
+              }
+            }
+            );
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+          });
+      };
+
+      async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+      }
+      upload(req).then((uploaded)=>{
+        processPost(uploaded.url);
+      });
+     }else{
+      processPost("");
+     }
+     function processPost(imageUrl){
+      req.body.featureImage = imageUrl;
+      blog_service.addPost(req.body);
+      res.redirect("/posts");
+      }
+     } 
+  
+  )
+
+  
   app.use((req, res) => {
     res.status(404).end('404 PAGE NOT FOUND');
 });
 
 
-
-  app.get("/")
-
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+blog_service.initialize().then(()=>{
+    console.log("initialize.then");
+    app.listen(HTTP_PORT, onHttpStart);
+    }).catch(err => {console.log(err);                              
+})
